9bd38741fff66ded1daeb17ca5d871b8a42017b5
Vankudoth-Swapna <vankudothswapna192@gmail.com>
2026-04-14 10:22:23 +0530

    modified day2

--- Day2/script.js
+++ Day2/script.js
@@ -1,42 +1,47 @@
-var a = 10;
-var a = 20;
-console.log(a);
+// var a =10;
+// var a =20;
+// console.log(a);
 
-let c = 32;
-console.log(c);
+ //let c = 32;
+ //c=67
+ //console.log(c);
 
-const b = 54;
-console.log(b);
+// const b = 54;
+// console.log(b);
 
-let num1 = 10;
-let num2 = "10";
-if(num1==num2){
-console.log("-1");
- }else{
-       console.log("1");
-    }
+// let num1 = 10;
+// let num2 = "10";
+// if(num1==num2){
+// console.log("-1");
+//  }else{
+//        console.log("1");
+//     }
 
-let x = (2*3)
-console.log(x);
-let y = (2**3)
-console.log(y);
+// let x = (2*3)
+// console.log(x);
+// let y = (2**3)
+// console.log(y);
 
 
 
-arrays
+// arrays
 
-let userData = [10,"hello",false,3.42,'bye'];
+// let userData = [10,"hello",false,3.42,'bye'];
 
-for(let index = 0;index < userData.length; index++){
-    console.log( userData[index]);
+// for(let index = 0;index < userData.length; index++){
+//     console.log( userData[index]);
     
-    // console.log(typeof userData[index]);  
-}
+//     // console.log(typeof userData[index]);  
+// }
 
 let userData1=[20,"hii",45,false];
 
-console.log(userData1);
-  userData1.push(10);
-  userData1.pop();
-  userData1.shift();
-  userData1.unshift("riya");
+//console.log(userData1);
+  //userData1.push(10);
+  
+//  userData1.pop();
+//  console.log(userData1);
+//   userData1.shift();
+//   console.log(userData1);
+ userData1.unshift("riya");
+ console.log(userData1);
