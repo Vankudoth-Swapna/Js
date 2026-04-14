1973a1de5ebd2b94c1f076246f6310ca3afde31c
Vankudoth-Swapna <vankudothswapna192@gmail.com>
2026-04-14 09:03:04 +0530

    added Day3

---
+++ Day1/script.js
@@ -0,0 +1,54 @@
+// console.log(-1);
+
+// var a=10;
+// var b=20;
+// if(a>b)
+// {
+//     console.log(a);
+// }
+// else
+// {
+//     console.log(b);
+// }
+
+// ==  => check values
+// === => check datatype and values
+// *   => multiplication
+// **  => power
+
+// var a = 10;
+// var b = "10"
+
+// if(a == b) console.log(-1);
+// if(a === ) console.log(-1);
+// console.log(2 ** 3)
+
+// var num1;
+// var num2 = 10;
+// var num3 = null;
+
+// console.log(num1);
+// console.log(typeof num1)
+
+// var num1 = 10
+// var num2 = 20
+// if(num1 < num2) {
+//     console.log(num1);
+// } else {
+//     console.log(num2);
+// }
+
+// var n = 10;
+// for(int i=0; i<=n; i++) {
+//     var fc=0;
+//     for(int j=1; j<=n; j++) {
+//         if(i%j == 0) {
+//             fc++;
+//         }
+//     }
+// }
+// if(fc == 2) {
+//     console.log("Prime Number");
+// } else {
+//     console.log("Not a Prime Number");
+// }
---
+++ Day3/script.js
@@ -0,0 +1,5 @@
+let name = "Swapna";
+let age = "21";
+
+console.log("My ... name is" + name + " i am " + age);
+console.log(`hi this is ${name} my age is ${age}`);
