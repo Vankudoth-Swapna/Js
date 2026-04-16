0c6f1f31915162da21f7bdf432021204b57b3983
Vankudoth-Swapna <vankudothswapna192@gmail.com>
2026-04-16 09:57:18 +0530

    added Day6

--- Day6/script.js
+++ Day6/script.js
@@ -1,20 +1,20 @@
 const add = () => {
     event.preventDefault()
 
-    let val1 = document.getElementById("value1").value 
-    let val2 = document.getElementById("value2")
+    let val1 = Number(document.getElementById("value1").value)
+    let val2 = Number(document.getElementById("value2").value)
     let display = document.getElementById("display")
 
-    display.innerHTML = `The sum of ${val1} add ${val2.value} is ${val1 + val2.value}`
+    display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 + val2}`
 }
 const sub = () => {
     event.preventDefault()
 
-    let val1 =Number(document.getElementById("value1").value)
+    let val1 =(document.getElementById("value1").value)
     let val2 = document.getElementById("value2")
     let display = document.getElementById("display")
 
-    display.innerHTML = `The sum of ${val1} add ${val2.value} is ${val1 - val2.value}`
+    display.innerHTML = `The sub of ${val1} and ${val2.value} is ${val1 - val2.value}`
 }
 const mul = () => {
     event.preventDefault()
@@ -23,7 +23,7 @@ const mul = () => {
     let val2 = document.getElementById("value2")
     let display = document.getElementById("display")
 
-    display.innerHTML = `The sum of ${val1} add ${val2.value} is ${val1 * val2.value}`
+    display.innerHTML = `The mul of ${val1} and ${val2.value} is ${val1 * val2.value}`
 }
 const div = () => {
     event.preventDefault()
@@ -32,6 +32,6 @@ const div = () => {
     let val2 = document.getElementById("value2")
     let display = document.getElementById("display")
 
-    display.innerHTML = `The sum of ${val1} add ${val2.value} is ${val1 / val2.value}`
+    display.innerHTML = `The div of ${val1} and ${val2.value} is ${val1 / val2.value}`
 }
 
